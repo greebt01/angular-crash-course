bd85f9456e6db7285726e2d1db46b8235864ee40
Birk <rgeebt01@yahoo.com>
2022-06-13 23:10:12 -0400

    added 1 and 2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  hello: string = 'Hello World!';
+//this disables/enables typing in the input box; this is property binding
+  isDisabled: boolean = true;
+//this is an event binding
+  onClick() {
+    alert("Button has been clicked!");
+  }
 }
